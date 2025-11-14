e6406a71c6536b82a8513d35dd74d252f18cd4a8
eduardofae <eduardofaeef@gmail.com>
2025-11-14 01:33:45 -0300

    Reorganização da atividade

--- src/plot.py
+++ src/plot.py
@@ -1,15 +1,11 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import json
-
-with open('./results.json') as file:
+with open('../results/results.json') as file:
     results = json.loads(file.read())
 
+import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 ax.boxplot(results.values(), labels=results.keys())
-
 ax.set_title('Steps to 10 successes')
 ax.set_xlabel('Agent')
 ax.set_ylabel('Steps')
-
 plt.show()
--- src/run.py
+++ src/run.py
@@ -4,12 +4,9 @@ N_SUCCESSES = 10
 N_AGENTS = 5
 
 import gymnasium as gym
-from tqdm import tqdm
-from sarsa_linear_agent import LinearSarsaAgent
-from feature_extractors import RBFFeatureExtractor
-import numpy as np
-
 env = gym.make('MountainCar-v0')
+
+from feature_extractors import RBFFeatureExtractor
 fes = [
     RBFFeatureExtractor(env), # N_CENTERS 25
     RBFFeatureExtractor(env, n_centers=10),
@@ -19,6 +16,7 @@ fes = [
     RBFFeatureExtractor(env, n_centers=48)
 ]
 
+from sarsa_linear_agent import LinearSarsaAgent
 agents = {
     'LR=0.004'  : LinearSarsaAgent(env, fes[0], learning_rate=0.004),
     'LR=0.02'   : LinearSarsaAgent(env, fes[0], learning_rate=0.02),
@@ -32,27 +30,32 @@ agents = {
     'CENTERS=48': LinearSarsaAgent(env, fes[5])
 }
 
-results = {
-    'LR=0.004'  : [],
-    'LR=0.02'   : [],
-    'LR=0.03'   : [],
-    'LR=0.06'   : [],
-    'LR=0.2'    : [],
-    'CENTERS=10': [],
-    'CENTERS=15': [],
-    'CENTERS=26': [],
-    'CENTERS=37': [],
-    'CENTERS=48': []
-}
+def get_results_dict_layout():
+    return {
+        feature: []
+        for feature in agents.keys()
+    }
 
-for seed in tqdm(range(N_REPETITIONS), desc='Repetitions'):
-    env.action_space.seed(seed)
-    for key, agent in tqdm(agents.items()):
-        _, _, steps_needed = agent.train_until_x_successes(N_STEPS, N_SUCCESSES, seed)
-        results[key].append(steps_needed)
+from tqdm import tqdm
+def gen_results():
+    results = get_results_dict_layout()
+    for seed in tqdm(range(N_REPETITIONS), desc='Repetitions'):
+        env.action_space.seed(seed)
+        for key, agent in tqdm(agents.items(), desc='Agents'):
+            _, _, steps_needed = agent.train_until_x_successes(N_STEPS, N_SUCCESSES, seed)
+            results[key].append(steps_needed)
+    return results
 
 import json
+from pathlib import Path
+def save_results(results, path='../results/results.json'):
+    path = Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, 'w') as file:
+        file.write(json.dumps(results))
+
+import numpy as np
+results = gen_results()
 sorted_results = sorted(results.items(), key=lambda item: np.median(item[1]))
 best_results = dict(sorted_results[:N_AGENTS])
-with open('./results.json', 'w') as file:
-    file.write(json.dumps(best_results))
+save_results(best_results)
